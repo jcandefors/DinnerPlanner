d918ff638cf6e6a19bdac4bad1907e6be8f53c75
Joakim Candefors <joakim.candefors@gmail.com>
2014-02-08 10:49:25 +0100

    Fixed updating images from model.

--- src/se/kth/csc/iprog/dinnerplanner/android/view/SummaryView.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/SummaryView.java
@@ -7,13 +7,9 @@ import se.kth.csc.iprog.dinnerplanner.R;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 import se.kth.csc.iprog.dinnerplanner.model.Dish;
 import se.kth.csc.iprog.dinnerplanner.model.Ingredient;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
-
 
 public class SummaryView {
 	View view;
@@ -23,58 +19,61 @@ public class SummaryView {
 	Dish desert;
 	public static final int INGREDIENTS = 4, STARTER = 1, MAIN = 2, DESERT = 3;
 
-
 	public SummaryView(View view, DinnerModel model) {
 
 		this.model = model;
 		starter = model.getSelectedDish(STARTER);
 		main = model.getSelectedDish(MAIN);
 		desert = model.getSelectedDish(DESERT);
+
 		// store in the class the reference to the Android View
 		this.view = view;
 
+		// Display total cost in the Summary
 		TextView totalcost = (TextView) view.findViewById(R.id.totalcost_text);
-
-
 		totalcost.setText(view.getResources().getString(R.string.total_cost)+" "+ model.getTotalMenuPrice() + " kr");
 
-		ImageButton instrbutton1 = (ImageButton) view.findViewById(R.id.instrButton1);
-		TextView instrtext1 = (TextView) view.findViewById(R.id.instrText1_id);
-		if(starter != null){			
-			instrbutton1.setImageResource(view.getResources().getIdentifier(starter.getImage(), "Drawable", this.getClass().getPackage().toString()));			
-			instrbutton1.setClickable(true);
+
+		// Get Starter ImageButton and TextView
+		ImageButton instrbutton1 = (ImageButton) view.findViewById(R.id.instrButton1);		
+		TextView instrtext1 = (TextView) view.findViewById(R.id.instrText1_id);		
+
+		// Change TextView below to Starter text
+		if(starter != null){		
+			instrbutton1.setImageResource(view.getResources().getIdentifier(starter.getImage(), "drawable", "se.kth.csc.iprog.dinnerplanner"));
 			instrtext1.setText(starter.getName());
 		}else{
 			instrbutton1.setImageResource(R.drawable.img_holder);
 			instrbutton1.setClickable(false);
 		}
 
-
+		// Get Main ImageButton and TextView
 		ImageButton instrbutton2 = (ImageButton) view.findViewById(R.id.instrButton2);
 		TextView instrtext2 = (TextView) view.findViewById(R.id.instrText2_id);
+
+		// Change TextView below to Main text
 		if(main != null){
-			//TODO läsa in bilderna
-			instrbutton1.setImageResource(view.getResources().getIdentifier(main.getImage(), "Drawable", this.getClass().getPackage().toString()));
+			instrbutton2.setImageResource(view.getResources().getIdentifier(main.getImage(), "drawable", "se.kth.csc.iprog.dinnerplanner"));
 			instrtext2.setText(main.getName());
 		}else{
 			instrbutton2.setClickable(false);
 			instrbutton2.setImageResource(R.drawable.img_holder);
 		}
 
-
+		// Get Desert ImageButton and TextView
 		ImageButton instrbutton3 = (ImageButton) view.findViewById(R.id.instrButton3);
 		TextView instrtext3 = (TextView) view.findViewById(R.id.instrText3_id);
+
+		// Change TextView below to Desert text
 		if(desert != null){
-			instrbutton3.setImageDrawable(Drawable.createFromPath("@drawable/"+desert.getImage()));
+			instrbutton3.setImageResource(view.getResources().getIdentifier(desert.getImage(), "drawable", "se.kth.csc.iprog.dinnerplanner"));
 			instrtext3.setText(desert.getName());
 		}else{
 			instrbutton3.setClickable(false);
 			instrbutton3.setImageResource(R.drawable.img_holder);
 		}
 
-
 		changeTab(INGREDIENTS);
-
 	}
 
 
