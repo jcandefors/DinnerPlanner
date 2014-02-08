cffba07d9aa5186e51d309fe51bc6b7424a01f2a
Victor <victordahlin@gmail.com>
2014-02-08 09:20:52 +0100

    Added real picture to summary onload

--- src/se/kth/csc/iprog/dinnerplanner/android/view/SummaryView.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/SummaryView.java
@@ -13,8 +13,6 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-
-
 public class SummaryView {
 	View view;
 	DinnerModel model;
@@ -23,58 +21,62 @@ public class SummaryView {
 	Dish desert;
 	public static final int INGREDIENTS = 4, STARTER = 1, MAIN = 2, DESERT = 3;
 
-
 	public SummaryView(View view, DinnerModel model) {
 
 		this.model = model;
 		starter = model.getSelectedDish(STARTER);
 		main = model.getSelectedDish(MAIN);
 		desert = model.getSelectedDish(DESERT);
+		
 		// store in the class the reference to the Android View
 		this.view = view;
 
+		// Display total cost in the Summary
 		TextView totalcost = (TextView) view.findViewById(R.id.totalcost_text);
-
-
 		totalcost.setText(view.getResources().getString(R.string.total_cost)+" "+ model.getTotalMenuPrice() + " kr");
 
-		ImageButton instrbutton1 = (ImageButton) view.findViewById(R.id.instrButton1);
-		TextView instrtext1 = (TextView) view.findViewById(R.id.instrText1_id);
-		if(starter != null){			
-			instrbutton1.setImageResource(view.getResources().getIdentifier(starter.getImage(), "Drawable", this.getClass().getPackage().toString()));			
+		
+		// Get Starter ImageButton and TextView
+		ImageButton instrbutton1 = (ImageButton) view.findViewById(R.id.instrButton1);		
+		TextView instrtext1 = (TextView) view.findViewById(R.id.instrText1_id);		
+		
+		// Change TextView below to Starter text
+		if(starter != null){		
+			instrbutton1.setImageDrawable(view.getResources().getDrawable(R.drawable.toast));
 			instrbutton1.setClickable(true);
 			instrtext1.setText(starter.getName());
 		}else{
 			instrbutton1.setImageResource(R.drawable.img_holder);
 			instrbutton1.setClickable(false);
 		}
-
-
+		
+		// Get Main ImageButton and TextView
 		ImageButton instrbutton2 = (ImageButton) view.findViewById(R.id.instrButton2);
 		TextView instrtext2 = (TextView) view.findViewById(R.id.instrText2_id);
+		
+		// Change TextView below to Main text
 		if(main != null){
-			//TODO läsa in bilderna
-			instrbutton1.setImageResource(view.getResources().getIdentifier(main.getImage(), "Drawable", this.getClass().getPackage().toString()));
+			instrbutton2.setImageDrawable(view.getResources().getDrawable(R.drawable.meatballs));
 			instrtext2.setText(main.getName());
 		}else{
 			instrbutton2.setClickable(false);
 			instrbutton2.setImageResource(R.drawable.img_holder);
 		}
-
-
+		
+		// Get Desert ImageButton and TextView
 		ImageButton instrbutton3 = (ImageButton) view.findViewById(R.id.instrButton3);
 		TextView instrtext3 = (TextView) view.findViewById(R.id.instrText3_id);
+
+		// Change TextView below to Desert text
 		if(desert != null){
-			instrbutton3.setImageDrawable(Drawable.createFromPath("@drawable/"+desert.getImage()));
+			//ImageButton instrbutton3 = (ImageButton) view.findViewById(R.id.instrButton2);
 			instrtext3.setText(desert.getName());
 		}else{
 			instrbutton3.setClickable(false);
 			instrbutton3.setImageResource(R.drawable.img_holder);
 		}
 
-
 		changeTab(INGREDIENTS);
-
 	}
 
 
